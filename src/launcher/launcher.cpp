0cf757cfc821ad5834a7594844cda946a6770495
Fleynaro <dariusjohnson555@gmail.com>
2022-05-06 18:03:28 +0500

    Project structure making

--- src/launcher/launcher.cpp
+++ src/launcher/launcher.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <CoreModule.hpp>
 #include <DatabaseModule.hpp>
 
 int main(int argc, char *argv[])
@@ -6,9 +7,10 @@ int main(int argc, char *argv[])
     using namespace sda;
 
     Program program;
+    program.addModule(CoreModule::Create());
     program.addModule(DatabaseModule::Create());
 
     auto project = Project::Create(&program, "Test");
-    
+
     return 0;
 }
