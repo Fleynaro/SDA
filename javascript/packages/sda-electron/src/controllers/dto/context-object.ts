5a793bd4190e3e0cda132dd61106f017cac11aaf
Fleynaro <dariusjohnson555@gmail.com>
2022-11-19 22:44:26 +0500

    electron js support implemented: remove toContextObjectId

--- javascript/packages/sda-electron/src/controllers/dto/context-object.ts
+++ javascript/packages/sda-electron/src/controllers/dto/context-object.ts
@@ -1,25 +1,6 @@
 import { toId } from '../../utils/common';
 import { ContextObject } from 'sda-core/object';
 import { ContextObject as ContextObjectDTO } from '../../api/context';
-// import { ObjectId } from '../../api/common';
-// import { AddressSpace } from 'sda-core/address-space';
-// import { toAddressSpaceId } from './address-space';
-// import { Image } from 'sda-core/image';
-// import { toImageId } from './image';
-
-// const ContextObjectToId: [any, (obj: any) => ObjectId][] = [
-//     [AddressSpace, toAddressSpaceId],
-//     [Image, toImageId],
-// ];
-
-// export const toContextObjectId = (obj: ContextObject): ObjectId => {
-//     const pair = ContextObjectToId.find(([cls]) => obj instanceof cls);
-//     if (!pair) {
-//         throw new Error(`Cannot find id for object ${obj}`);
-//     }
-//     const [_, toId] = pair;
-//     return toId(obj);
-// };
 
 export const toContextObjectDTO = (obj: ContextObject): ContextObjectDTO => {
     return {
