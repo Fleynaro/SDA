4b0dba65e675b170361750992d8113244d4984ab
Fleynaro <dariusjohnson555@gmail.com>
2023-03-30 00:18:06 +0500

    ir-code - add xmm if-else test

--- core/test/test/IRcode/IRcodeTest.cpp
+++ core/test/test/IRcode/IRcodeTest.cpp
@@ -142,6 +142,69 @@ TEST_F(IRcodeTest, IfElseConditionAlAh) {
     ASSERT_TRUE(cmp(function, expectedIRode));
 }
 
+TEST_F(IRcodeTest, IfElseConditionXmm) {
+    // Graph: https://photos.app.goo.gl/MyxDwPBjKYKYK13P9
+    auto sourcePCode = "\
+        xmm0:Da = COPY 1:4 \n\
+        xmm0:Db = COPY 2:4 \n\
+        xmm0:Dc = COPY 3:4 \n\
+        xmm0:Dd = COPY 4:4 \n\
+        CBRANCH <labelElse>, 0:1 // if-else condition \n\
+        xmm0:Da = INT_ADD xmm0:Da, 1:4 // then block \n\
+        xmm0:Db = INT_ADD xmm0:Db, 2:4 \n\
+        xmm0:Dc = INT_ADD xmm0:Dc, 3:4 \n\
+        xmm0:Dd = INT_ADD xmm0:Dd, 4:4 \n\
+        BRANCH <labelEnd> \n\
+        <labelElse>: \n\
+        xmm0:Da = INT_SUB xmm0:Da, 1:4 // else block \n\
+        xmm0:Db = INT_SUB xmm0:Db, 2:4 \n\
+        xmm0:Dc = INT_SUB xmm0:Dc, 3:4 \n\
+        xmm0:Dd = INT_SUB xmm0:Dd, 4:4 \n\
+        <labelEnd>: \n\
+        rax:8 = INT_ADD rsp:8, 0:8 \n\
+        STORE rax:8, xmm0:Da \n\
+        rax:8 = INT_ADD rsp:8, 4:8 \n\
+        STORE rax:8, xmm0:Db \n\
+        rax:8 = INT_ADD rsp:8, 8:8 \n\
+        STORE rax:8, xmm0:Dc \n\
+        rax:8 = INT_ADD rsp:8, 12:8 \n\
+        STORE rax:8, xmm0:Dd \
+    ";
+    auto expectedIRode = "\
+        Block B0(level: 1, near: B5, far: Ba): \n\
+            var1[xmm0]:4 = COPY 0x1:4 \n\
+            var2[xmm0 + 4]:4 = COPY 0x2:4 \n\
+            var3[xmm0 + 8]:4 = COPY 0x3:4 \n\
+            var4[xmm0 + 12]:4 = COPY 0x4:4 \n\
+        Block B5(level: 2, far: Be): \n\
+            var5[xmm0]:4 = INT_ADD var1, 0x1:4 \n\
+            var6[xmm0 + 4]:4 = INT_ADD var2, 0x2:4 \n\
+            var7[xmm0 + 8]:4 = INT_ADD var3, 0x3:4 \n\
+            var8[xmm0 + 12]:4 = INT_ADD var4, 0x4:4 \n\
+        Block Ba(level: 2, near: Be): \n\
+            var9[xmm0]:4 = INT_SUB var1, 0x1:4 \n\
+            var10[xmm0 + 4]:4 = INT_SUB var2, 0x2:4 \n\
+            var11[xmm0 + 8]:4 = INT_SUB var3, 0x3:4 \n\
+            var12[xmm0 + 12]:4 = INT_SUB var4, 0x4:4 \n\
+        Block Be(level: 3): \n\
+            var13:8 = LOAD rsp \n\
+            var14[rax]:8 = INT_ADD var13, 0x0:8 \n\
+            var15:4 = PHI var5, var9 \n\
+            var16[var13]:4 = COPY var15 \n\
+            var17[rax]:8 = INT_ADD var13, 0x4:8 \n\
+            var18:4 = PHI var6, var10 \n\
+            var19[var13 + 4]:4 = COPY var18 \n\
+            var20[rax]:8 = INT_ADD var13, 0x8:8 \n\
+            var21:4 = PHI var7, var11 \n\
+            var22[var13 + 8]:4 = COPY var21 \n\
+            var23[rax]:8 = INT_ADD var13, 0xc:8 \n\
+            var24:4 = PHI var8, var12 \n\
+            var25[var13 + 12]:4 = COPY var24 \
+    ";
+    auto function = parsePcode(sourcePCode, &program);
+    ASSERT_TRUE(cmp(function, expectedIRode));
+}
+
 TEST_F(IRcodeTest, MultipleParentBlocksNoPhi) {
     // Graph: https://photos.app.goo.gl/UsYTBTCBcx4hm5jT8
     auto sourcePCode = "\
