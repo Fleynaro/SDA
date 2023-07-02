19fd21e6dcf3bbe43144e2c6cd99f52e6f5d6672
Fleynaro <dariusjohnson555@gmail.com>
2023-07-02 19:44:09 +0500

    extract ConstConditionSemanticsFixture

--- core/test/test/Semantics/ConstConditionSemanticsTest.cpp
+++ core/test/test/Semantics/ConstConditionSemanticsTest.cpp
@@ -1,48 +1,10 @@
-#include "Test/Core/Semantics/SemanticsFixture.h"
-#include "SDA/Core/Semantics/ConstConditionSemantics.h"
+#include "Test/Core/Semantics/ConstConditionSemanticsFixture.h"
 
 using namespace sda;
 using namespace sda::test;
 using namespace ::testing;
 
-class ConstConditionSemanticsTest : public SemanticsFixture
-{
-protected:
-    std::unique_ptr<semantics::ConstConditionRepository> constCondRepo;
-
-    void SetUp() override {
-        IRcodeFixture::SetUp();
-        constCondRepo = std::make_unique<semantics::ConstConditionRepository>(program);
-        eventPipe->connect(constCondRepo->getEventPipe());
-    }
-
-    ::testing::AssertionResult cmpConditions(ircode::Function* function, const std::string& expectedCode) const {
-        std::stringstream ss;
-        utils::AbstractPrinter printer;
-        printer.setOutput(ss);
-        for (size_t i = 0; i < 2; ++i)
-            printer.startBlock();
-        printer.newTabs();
-        auto blockInfos = function->getFunctionGraph()->getBlocks(true);
-        for (auto& [pcodeBlock, level] : blockInfos) {
-            auto block = function->toBlock(pcodeBlock);
-            auto conditions = constCondRepo->findConditions(block);
-            if (!conditions.empty()) {
-                ss << "Block " << block->getName() << ":";
-                printer.startBlock();
-                for (auto& cond : conditions) {
-                    printer.newLine();
-                    ss << cond.var->getName();
-                    ss << (cond.type == semantics::ConstantCondition::EQUAL ? " == " : " != ");
-                    ss << cond.value;
-                }
-                printer.endBlock();
-                printer.newLine();
-            }
-        }
-        return Compare(ss.str(), expectedCode);
-    }
-};
+class ConstConditionSemanticsTest : public ConstConditionSemanticsFixture {};
 
 TEST_F(ConstConditionSemanticsTest, IfElse) {
     auto sourcePCode = "\
