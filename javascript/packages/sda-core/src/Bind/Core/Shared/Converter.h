432529d1a4f5d502545c3bed7fd4eba4c71ffa0f
Fleynaro <dariusjohnson555@gmail.com>
2023-07-28 14:49:58 +0500

    add converter for boost::json::object

--- javascript/packages/sda-core/src/Bind/Core/Shared/Converter.h
+++ javascript/packages/sda-core/src/Bind/Core/Shared/Converter.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <boost/json.hpp>
 #include "SDA/Core/Pcode/PcodeInstruction.h"
 
 namespace v8pp
@@ -26,6 +27,30 @@ namespace v8pp
 
         static to_type to_v8(v8::Isolate* isolate, const from_type& value) {
             return convert<underlying_type>::to_v8(isolate, value);
+        }
+    };
+
+    // for boost::json::object
+    template<>
+    struct convert<boost::json::object>
+    {
+        using from_type = boost::json::object;
+        using to_type = v8::Local<v8::Value>;
+
+        static bool is_valid(v8::Isolate* isolate, v8::Local<v8::Value> value) {
+            return !value.IsEmpty() && value->IsObject();
+        }
+
+        static from_type from_v8(v8::Isolate* isolate, v8::Local<v8::Value> value) {
+            if (!is_valid(isolate, value)) {
+                throw std::invalid_argument("expected non-empty object for boost::json::object");
+            }
+            auto json = v8pp::json_str(isolate, value);
+            return boost::json::parse(json).as_object();
+        }
+
+        static to_type to_v8(v8::Isolate* isolate, const from_type& value) {
+            return v8pp::json_parse(isolate, boost::json::serialize(value));
         }
     };
 };
--- javascript/packages/sda-core/src/Bind/Core/Utils.h
+++ javascript/packages/sda-core/src/Bind/Core/Utils.h
@@ -10,18 +10,10 @@ namespace sda::bind
         static auto Serialize(utils::ISerializable* obj) {
             boost::json::object data;
             obj->serialize(data);
-            auto isolate = v8::Isolate::GetCurrent();
-            return v8pp::json_parse(isolate, boost::json::serialize(data));
+            return data;
         }
 
-        static void Deserialize(utils::ISerializable* obj, v8::Local<v8::Value> value) {
-            auto isolate = v8::Isolate::GetCurrent();
-            if (!value->IsObject()) {
-                v8pp::throw_ex(isolate, "Invalid argument: expected object");
-                return;
-            }
-            auto json = v8pp::json_str(isolate, value);
-            boost::json::object data = boost::json::parse(json).as_object();
+        static void Deserialize(utils::ISerializable* obj, boost::json::object& data) {
             obj->deserialize(data);
         }
     public:
