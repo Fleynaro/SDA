9fe8251439022bb9c8f8a7e97e8e2437977a3b9c
Fleynaro <dariusjohnson555@gmail.com>
2023-08-08 00:38:14 +0500

    add env var DISABLE_SDA_LOGS

--- core/src/Utils/Logger.cpp
+++ core/src/Utils/Logger.cpp
@@ -1,6 +1,12 @@
 #include "SDA/Core/Utils/Logger.h"
 #include <plog/Initializers/RollingFileInitializer.h>
+#include <cstdlib>
 
 void utils::InitLogger() {
+    if (auto value = std::getenv("DISABLE_SDA_LOGS")) {
+        if (std::string(value) == "1") {
+            return;
+        }
+    }
     plog::init(plog::debug, "logs.txt");
 }
