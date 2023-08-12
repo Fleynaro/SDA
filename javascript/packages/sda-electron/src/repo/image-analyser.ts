41d7bfa0eb63a3328ba03b786bbec958569c669a
Fleynaro <dariusjohnson555@gmail.com>
2023-08-12 15:02:03 +0500

    rename analysers -> analyzers

--- javascript/packages/sda-electron/src/repo/image-analyser.ts
+++ javascript/packages/sda-electron/src/repo/image-analyser.ts
@@ -1,22 +1,22 @@
 import { ImageAnalyser, PEImageAnalyser } from 'sda-core';
 
-const analyser: { [name: string]: ImageAnalyser } = {};
+const analyzers: { [name: string]: ImageAnalyser } = {};
 
 export const findImageAnalyser = (name: string): ImageAnalyser => {
-  if (name in analyser) {
-    return analyser[name];
+  if (name in analyzers) {
+    return analyzers[name];
   }
   throw new Error(`Image analyser ${name} not found`);
 };
 
-export const getImageAnalysers = (): ImageAnalyser[] => {
-  return Object.values(analyser);
+export const getImageAnalyzers = (): ImageAnalyser[] => {
+  return Object.values(analyzers);
 };
 
 export const addImageAnalyser = (imageAnalyser: ImageAnalyser) => {
-  analyser[imageAnalyser.name] = imageAnalyser;
+  analyzers[imageAnalyser.name] = imageAnalyser;
 };
 
-export const initDefaultImageAnalysers = () => {
+export const initDefaultImageAnalyzers = () => {
   addImageAnalyser(PEImageAnalyser.New());
 };
