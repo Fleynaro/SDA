f52abf2a9c626b5a50532689ecd8ed54dea06a2c
Fleynaro <dariusjohnson555@gmail.com>
2023-08-27 20:22:23 +0500

    introduce TokenizedTextView.tsx

--- javascript/packages/sda-gui/src/components/TokenizedTextView/TokenizedTextView.tsx
+++ javascript/packages/sda-gui/src/components/TokenizedTextView/TokenizedTextView.tsx
@@ -1,36 +1,220 @@
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { Grid } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { Token, TokenizedText } from 'sda-electron/api/common';
+import { PcodeStructBlockTokenGroupAction, PcodeTokenGroupAction } from 'sda-electron/api/p-code';
+import { useHtmlTextSelection } from 'components/Text';
+
+const row = {
+  height: 20,
+};
+
+const useStyles = makeStyles(() => ({
+  root: {
+    cursor: 'default',
+    userSelect: 'none',
+    whiteSpace: 'nowrap', // prevent line breaks
+  },
+  leftColumn: {
+    width: 50,
+    color: '#999999',
+  },
+  lineIndexColumn: {
+    textAlign: 'right',
+  },
+  collapseCodeColumn: {
+    width: 20,
+    color: '#a8a8a8',
+    opacity: 0,
+    transition: 'opacity 0.2s ease-in-out',
+    '&:hover': {
+      opacity: 1,
+    },
+  },
+  collapseButton: {
+    cursor: 'pointer',
+  },
+  collapseEllipsis: {
+    cursor: 'pointer',
+    color: '#999999',
+  },
+  row,
+  codeRow: {
+    ...row,
+    userSelect: 'text',
+  },
+}));
 
 interface TokenizedTextViewProps {
+  name: string;
   text: TokenizedText;
+  tokenTypeToColor?: { [type: string]: string };
+  highlightedGroupIdxs?: number[];
 }
 
-export const TokenizedTextView = ({ text }: TokenizedTextViewProps) => {
-  const linesOfTokens = useMemo(() => {
-    const linesOfTokens: Token[][] = [[]];
+interface Line {
+  index: number;
+  tokens: Token[];
+  collapse?: {
+    id: string;
+    endLine: Line;
+  };
+}
+
+export const TokenizedTextView = ({
+  name,
+  text,
+  tokenTypeToColor = {},
+  highlightedGroupIdxs = [],
+}: TokenizedTextViewProps) => {
+  const classes = useStyles();
+  const [collapsedLineIds, setCollapsedLineIds] = useState<string[]>([]);
+  const { addExtractor } = useHtmlTextSelection();
+
+  useEffect(() => {
+    return addExtractor((e) => {
+      if (e.dataset.area === name) {
+        return text.groups[Number(e.dataset.groupIdx)];
+      }
+      return null;
+    });
+  }, [addExtractor, name, text.groups]);
+
+  // all lines that can be shown
+  const allLines = useMemo(() => {
+    const lines: Line[] = [];
+    // create lines
     for (const token of text.tokens) {
-      linesOfTokens[linesOfTokens.length - 1].push(token);
-      if (token.text === '\n') {
-        linesOfTokens.push([]);
+      if (lines.length === 0 || token.text === '\n') {
+        lines.push({ index: lines.length, tokens: [] });
+        if (token.text === '\n') continue;
+      }
+      lines[lines.length - 1].tokens.push(token);
+    }
+    // find lines that can be collapsed
+    for (const line of lines) {
+      const token = line.tokens.find(
+        (token) =>
+          token.type === 'Keyword' &&
+          text.groups[token.groupIdx].action.name === PcodeTokenGroupAction.StructBlock,
+      );
+      if (token) {
+        const endLine = lines.find(
+          (l) =>
+            l.index > line.index + 1 &&
+            l.tokens.find((t) => t.type === 'Keyword' && t.groupIdx === token.groupIdx),
+        );
+        if (endLine) {
+          const { id } = text.groups[token.groupIdx].action as PcodeStructBlockTokenGroupAction;
+          line.collapse = { id, endLine };
+        }
       }
     }
-    return linesOfTokens;
+    return lines;
   }, [text.tokens]);
+
+  // lines that are not hidden (collapsed)
+  const lines = useMemo(() => {
+    const hiddenIndexes = new Set<number>();
+    // hide if line is in collapsed block
+    for (const id of collapsedLineIds) {
+      const line = allLines.find((l) => l.collapse?.id === id);
+      if (line && line.collapse) {
+        for (let i = line.index + 1; i < line.collapse.endLine.index; i++) {
+          hiddenIndexes.add(i);
+        }
+      }
+    }
+    // ...
+    return allLines.filter((l) => !hiddenIndexes.has(l.index));
+  }, [collapsedLineIds, allLines]);
+
+  const lineHighlightColor = useMemo(() => {
+    const lineHighlightColor = new Map<number, string>();
+    for (const line of allLines) {
+      if (line.tokens.find((t) => highlightedGroupIdxs.includes(t.groupIdx))) {
+        lineHighlightColor.set(line.index, '#304559');
+      }
+    }
+    return lineHighlightColor;
+  }, [allLines, highlightedGroupIdxs]);
+
+  const onCollapseCode = useCallback(
+    (line: Line) => {
+      if (!line.collapse) return;
+      const { id } = line.collapse;
+      if (collapsedLineIds.includes(id)) {
+        setCollapsedLineIds(collapsedLineIds.filter((l) => l !== id));
+      } else {
+        setCollapsedLineIds([...collapsedLineIds, id]);
+      }
+    },
+    [lines],
+  );
+
+  const isLineCollapsed = useCallback(
+    (line: Line) => (line.collapse ? collapsedLineIds.includes(line.collapse.id) : false),
+    [collapsedLineIds],
+  );
 
   const content = useMemo(() => {
     return (
-      <Grid container direction="column">
-        {linesOfTokens.map((tokens, i) => (
-          <Grid item key={i}>
-            {tokens.map((token, j) => (
-              <span key={j}>{token.text}</span>
+      <Grid container direction="row" wrap="nowrap" className={classes.root}>
+        <Grid item container direction="row" wrap="nowrap" className={classes.leftColumn}>
+          <Grid item container direction="column" className={classes.lineIndexColumn}>
+            {lines.map((line) => (
+              <Grid item key={line.index} className={classes.row}>
+                {line.index + 1}
+              </Grid>
             ))}
           </Grid>
-        ))}
+          <Grid item container direction="column" className={classes.collapseCodeColumn}>
+            {lines.map((line) => (
+              <Grid item key={line.index} className={classes.row}>
+                {line.collapse && (
+                  <div onClick={() => onCollapseCode(line)} className={classes.collapseButton}>
+                    {isLineCollapsed(line) ? <KeyboardArrowRightIcon /> : <ExpandMoreIcon />}
+                  </div>
+                )}
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+        <Grid item container direction="column">
+          {lines.map((line) => (
+            <Grid
+              item
+              direction="row"
+              className={classes.codeRow}
+              key={line.index}
+              style={{ backgroundColor: lineHighlightColor.get(line.index) }}
+            >
+              {line.tokens.map((token, j) => (
+                <span
+                  key={j}
+                  style={{ color: tokenTypeToColor[token.type] }}
+                  data-group-idx={token.groupIdx}
+                  data-area={name}
+                >
+                  {token.text === ' ' ? '\u00A0' : token.text}
+                </span>
+              ))}
+              {isLineCollapsed(line) && (
+                <>
+                  {' '}
+                  <span className={classes.collapseEllipsis} onClick={() => onCollapseCode(line)}>
+                    ...
+                  </span>
+                </>
+              )}
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
     );
-  }, [linesOfTokens]);
+  }, [lines, lineHighlightColor, isLineCollapsed]);
 
   return content;
 };
