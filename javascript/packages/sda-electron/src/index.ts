a026846fd22d908c2fc9d39f7dbf1dcf24fe6259
Fleynaro <dariusjohnson555@gmail.com>
2023-08-27 20:01:03 +0500

    add react dev tools extension to electron

--- javascript/packages/sda-electron/src/index.ts
+++ javascript/packages/sda-electron/src/index.ts
@@ -1,10 +1,20 @@
-import { app } from 'electron';
+import { app, session } from 'electron';
+import isElectronDev from 'electron-is-dev';
 import { windowController } from './controllers';
 import sdaApp from './app';
 
 app.whenReady().then(async () => {
   sdaApp.init();
   windowController.openProjectManagerWindow();
+
+  // install useful chrome extensions
+  if (isElectronDev) {
+    // https://github.com/facebook/react/issues/25843#issuecomment-1406766561
+    // (download at SDA\javascript\packages\sda-electron\data\chrome-extension\ReactDevTools.zip)
+    await session.defaultSession.loadExtension('C:\\Users\\Fleynaro\\Downloads\\ReactDevTools', {
+      allowFileAccess: true,
+    });
+  }
 
   // app.on('activate', () => {
   //     if (BrowserWindow.getAllWindows().length === 0)
